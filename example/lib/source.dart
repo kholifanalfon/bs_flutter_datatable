89af40293a46a832c43e0fc84382be083e602908
jobdesc.kholifan <jobdesc.kholifanalfon@gmail.com>
2021-07-02 23:09:29 +0700

    ## 1.1.0
    * Property `pageLength` and `searchable` has been removed, to hide/show you can use `customizeLayout`
    * Adding properties `cutomizeLayout` to customize table, pageLength, and etc layout position
    * Property `controller` merge to property source class
    * Rename refresh function to reload
    * Make datatable source class to simply way, but class property and function still same
    * Change property type `pagination` from List<String> to List<BsPaginationButtons>
    * Adding class `BsPaginationButtons` to defined pagination buttons order
    * Rename class `BsPaginateButton` to `BsPaginationButton`
    * Rename class `BsPaginateButtonStyle` to `BsPaginationButtonStyle`
    * Fixed responsive and pagination issue

--- example/lib/source.dart
+++ example/lib/source.dart
@@ -1,31 +1,42 @@
+import 'package:bs_flutter_buttons/bs_flutter_buttons.dart';
 import 'package:bs_flutter_datatable/bs_flutter_datatable.dart';
 import 'package:flutter/material.dart';
 
 class ExampleSource extends BsDatatableSource {
-  BsDatatableResponse _response;
 
-  ExampleSource({
-    BsDatatableResponse response = const BsDatatableResponse(),
-  }) : _response = response;
+  ValueChanged<dynamic> onEditListener = (value) {};
+  ValueChanged<dynamic> onDeleteListener = (value) {};
 
-  @override
-  // TODO: implement countData
-  int get countData => _response.countData;
-
-  @override
-  // TODO: implement countDataPage
-  int get countDataPage => _response.data.length;
-
-  @override
-  // TODO: implement countFiltered
-  int get countFiltered => _response.countFiltered;
+  static List<BsDataColumn> get columns => <BsDataColumn>[
+    BsDataColumn(label: Text('No'), orderable: false, searchable: false, width: 100.0),
+    BsDataColumn(label: Text('Code'), columnName: 'typecd', width: 200.0),
+    BsDataColumn(label: Text('Name'), columnName: 'typenm'),
+    BsDataColumn(label: Text('Aksi'), orderable: false, searchable: false, width: 200.0),
+  ];
 
   @override
   BsDataRow getRow(int index) {
     return BsDataRow(index: index, cells: <BsDataCell>[
-      BsDataCell(Text('${_response.start + index + 1}')),
-      BsDataCell(Text('${_response.data[index]['typecd']}')),
-      BsDataCell(Text('${_response.data[index]['typenm']}')),
+      BsDataCell(Text('${controller.start + index + 1}')),
+      BsDataCell(Text('${response.data[index]['typecd']}')),
+      BsDataCell(Text('${response.data[index]['typenm']}')),
+      BsDataCell(Row(
+        children: [
+          BsButton(
+            margin: EdgeInsets.only(right: 5.0),
+            onPressed: () => onEditListener(response.data[index]['typecd']),
+            prefixIcon: Icons.edit,
+            size: BsButtonSize.btnIconSm,
+            style: BsButtonStyle.primary,
+          ),
+          BsButton(
+            onPressed: () => onEditListener(response.data[index]['typecd']),
+            prefixIcon: Icons.delete,
+            size: BsButtonSize.btnIconSm,
+            style: BsButtonStyle.danger,
+          )
+        ],
+      ))
     ]);
   }
 }
